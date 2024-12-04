c11a9e3be7f90a3192e714b25fb9e31b0da3d3d6
Liam Malone <maloneliam@proton.me>
2024-12-04 01:05:42 +0100

    Rename project

--- build.zig
+++ build.zig
@@ -61,7 +61,7 @@ pub fn build(b: *std.Build) !void {
     });
 
     const exe = b.addExecutable(.{
-        .name = "GfxDemo",
+        .name = "SimpGfx",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
