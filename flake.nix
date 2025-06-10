aacea49901311f29ddd3134b31d72aa5706c5294
Liam Malone <maloneliam@proton.me>
2025-06-11 00:02:49 +0200

    Update Zig compiler version, add man-pages

--- flake.nix
+++ flake.nix
@@ -1,9 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    zig.url = "github:mitchellh/zig-overlay";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, zig }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
@@ -14,9 +15,9 @@
       devShells = forEachSupportedSystem({ pkgs }: {
         default = pkgs.mkShell {
           packages = with pkgs; [
-            gdb
             lldb
             libinput
+            man-pages
             mesa
             shaderc
             valgrind
@@ -26,7 +27,7 @@
             vulkan-tools
             vulkan-headers
             vulkan-validation-layers
-            zig
+            zig.packages.${system}.master-2025-06-09
           ];
         };
       });
